517ad5c4deae900a4ce4fe929627714f697b7eff
Ryan Coppolo <r@coppolo.com>
2012-07-27 17:45:35 -0700

    Converts it to a basic express app.

--- server.js
+++ server.js
@@ -1,53 +1,46 @@
-var http = require('http');
+var express = require('express');
 
 var posts = {
-  '/welcome': {
+  'welcome': {
     title: 'How the hell are you?',
     body: 'Here is a blog post I guess.'
   }
 };
 
-var server = http.createServer(function(req, res) {
-  if (req.url === '/') {
-    index();
-  } else if (posts[req.url]) {
-    post(req.url);
-  } else {
-    notFound();
-  }
+var app = require('express').createServer();
 
-  function index() {
-    var s = "<title>My Blog</title>\n";
-    s += "<h1>My Blog</h1>\n";
-    s += "<ul>\n";
-    for (var slug in posts) {
-      var post = posts[slug];
-      s += '<li><a href="' + slug + '">' + post.title + '</a></li>' + "\n";
-    }
-    s += "</ul>\n";
-    sendBody(s);
+app.get('/', function(req,res) {
+  var s = "<title>My blog.</title>\n";
+  s += "<h1>My blog.</h1>\n";
+  s += "<ul>\n";
+  for (var slug in posts) {
+    var post = posts[slug];
+    s += '<li><a href="/posts/' + slug + '">' + post.title + '</a></li>' + "\n";
   }
-
-  function post(url) {
-    var post = posts[url];
-    var s = "<title>" + post.title + "</title>\n";
-    s += "<h1>My Blog</h1>\n";
-    s += "<h2>" + post.title + "</h2>\n";
-    s += post.body;
-    sendBody(s);
-  }
-
-  function notFound() {
-    res.writeHead(404,{'Content-Type': 'text/html'});
-    res.end('<h1>Post not found.</h1>');
-  }
-
-  function sendBody(s) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(s);
-  }
-
+  s += "</ul>\n";
+  res.send(s);
 });
 
-server.listen(3001);
+app.get('/posts/:slug', function(req,res) {
+  var post = posts[req.params.slug];
+  if (typeof(post) === 'undefined') {
+    notFound(res);
+    return;
+  }
+  var s = "<title>" + post.title + "</title>\n";
+  s += "<h1>My blog.</h1>\n";
+  s += "<h2>" + post.title + "</h2>\n";
+  s += post.body;
+  res.send(s);
+});
+
+app.get('*', function(req,res) {
+  notFound(res);
+});
+
+function notFound(res) {
+  res.send('<h1>Page not found.</h1>', 404);
+}
+
+app.listen(3001);
 console.log("Listening for connections.");
